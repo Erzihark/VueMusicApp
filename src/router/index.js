848b1dccd753558241a83f1196dcdc394eaf1da5
ManuelCarretero <manuel.sanchez@evolvemediallc.com>
2023-01-04 16:59:24 -0600

    Editing route params

--- src/router/index.js
+++ src/router/index.js
@@ -1,65 +1,69 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import useUserStore from "@/stores/user"
-import Home from "@/views/HomeView.vue"
-import About from "@/views/AboutView.vue"
-import Manage from "@/views/ManageView.vue"
-
+import { createRouter, createWebHistory } from "vue-router";
+import useUserStore from "@/stores/user";
+import Home from "@/views/HomeView.vue";
+import About from "@/views/AboutView.vue";
+import Manage from "@/views/ManageView.vue";
+import Song from "@/views/SongView.vue";
 
 const routes = [
   {
-    name: 'home',
+    name: "home",
     path: "/",
-    component: Home
+    component: Home,
   },
   {
-    name: 'about',
+    name: "about",
     path: "/about",
     component: About,
   },
   {
-    name: 'manage',
+    name: "manage",
     // alias: '/manage',
     path: "/manage-music",
     component: Manage,
-    beforeEnter: (to, from, next)=>{
-      console.log('manage route guard');
+    beforeEnter: (to, from, next) => {
+      console.log("manage route guard");
       next();
     },
     meta: {
       requiresAuth: true,
-    }
+    },
   },
   {
-    path: '/manage',
-    redirect: {name: 'manage'},
+    path: "/manage",
+    redirect: { name: "manage" },
   },
   {
-    path: '/:catchAll(.*)*',
-    redirect: {name: 'home'}
-  }
+    name: "song",
+    path: "/song/:id",
+    component: Song,
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  linkExactActiveClass: 'text-yellow-500'
-})
+  linkExactActiveClass: "text-yellow-500",
+});
 
-router.beforeEach((to, from, next)=>{
+router.beforeEach((to, from, next) => {
   //console.log(to.meta);
-  if(!to.meta.requiresAuth){
+  if (!to.meta.requiresAuth) {
     next();
     return;
   }
 
   const store = useUserStore();
 
-  if(store.userLoggedIn){
+  if (store.userLoggedIn) {
     next();
   } else {
-    next({name: "home"});
+    next({ name: "home" });
   }
-
 });
 
-export default router
+export default router;
